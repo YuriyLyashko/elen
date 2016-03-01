24dd185bf8b8dc6dff70bf8896ac226e64dd3890
YuriyLyashko <yu.i.lyashko@gmail.com>
2016-03-01 14:57:45 +0200

    modified updating and saving rates

--- start.py
+++ start.py
@@ -4,7 +4,7 @@ from datetime import * #tzinfo, date, timedelta
 
 #from tarifi import *
 #from launch_calc import *
-from saves import *
+#from saves import *
 #from graph import *
 
 import pickle
@@ -221,6 +221,45 @@ def launch_calc(event):
         calc(mt1, mt2, t1, t2, t3, l1, l2, k)
         
     
+#Функція, що викликається кнопкою "Зберегти в файл"
+def sortByAlphabet_Day(inputStr):
+#    print(inputStr[2:4])
+    return inputStr[2:4]
+def sortByAlphabet_Month(inputStr):
+#    print(inputStr[5:7])
+    return inputStr[5:7]
+def sortByAlphabet_Year(inputStr):
+#    print(inputStr[8:12])
+    return inputStr[8:12]
+
+def saves(event):
+    print('saves')
+    date = ent10.get()
+    h = open('history.txt', 'a')
+    if len(calc_end) < 2:
+        exec(open('error.py').read())
+    else:
+        h.write(str(calc_end)+"s \n")
+        h.close()
+        s = open('saves.txt', 'a')
+        #a - дозапис, 'w' - запис, 'r' - читання
+        s.write(str(calc_end)+"\n")
+        s.close()
+        s = open('saves.txt', 'r')
+        all_lines = s.readlines()
+        all_lines.sort(key = sortByAlphabet_Day)
+        all_lines.sort(key = sortByAlphabet_Month)
+        all_lines.sort(key = sortByAlphabet_Year)
+        n = 0
+        while n < len(all_lines)-1:
+            if all_lines[n][2:12] == all_lines[n+1][2:12]:
+                all_lines.pop(n)
+                n = n
+            else:
+                n += 1
+        s = open('saves.txt', 'w')
+        s.writelines(all_lines)
+        s.close()
 
    
 root.title("Розрахунок вартості спожитої електроенергії")
